463bfc9c75ac00bfd23af9eed62939f098a49b9b
AzrilFahmiardi <anakijummira@gmail.com>
2024-09-16 18:16:45 +0700

    ada env

--- server/db/database.js
+++ server/db/database.js
@@ -1,11 +1,23 @@
 const { Client } = require("pg");
+const dotenv = require("dotenv");
+const Sequelize = require("sequelize");
 
-const client = new Client({
-  user: "azril",
-  host: "dpg-crjvphdds78s73efk8i0-a",
-  database: "portfoliodb_fqoa",
-  password: "GX8FRhsZbraGgumRibNkGwHMySS3fnbs",
-  port: 5432,
+dotenv.config();
+
+const { DB_NAME, DB_PASSWORD, DB_USER, DB_URL } = process.env;
+
+// const client = new Client({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "portfolio",
+//   password: "12345678",
+//   port: 5432,
+// });
+
+const client = new Sequelize(DB_URL, {
+  define: {
+    timestamps: false,
+  },
 });
 
 module.exports = client;
