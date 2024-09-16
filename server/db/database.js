cbf0059ece1cfbe7f7a97e8935d87400f6e29d4c
AzrilFahmiardi <anakijummira@gmail.com>
2024-09-16 16:54:05 +0700

    database config

--- server/db/database.js
+++ server/db/database.js
@@ -1,10 +1,10 @@
 const { Client } = require("pg");
 
 const client = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: "portfolio",
-  password: "12345678",
+  user: "azril",
+  host: "dpg-crjvphdds78s73efk8i0-a",
+  database: "portfoliodb_fqoa",
+  password: "GX8FRhsZbraGgumRibNkGwHMySS3fnbs",
   port: 5432,
 });
 
